7e0f9f8ddd43148ab262f1971b67cbecc895c37b
amhauger <ahauger@dontdrivedirty.com>
2023-01-25 13:54:51 -0800

    feat: setup server

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"net/http"
 
 	"github.com/rs/zerolog/log"
+
+	"github.com/gin-gonic/gin"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
@@ -13,6 +18,46 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace"
 	"google.golang.org/grpc/credentials"
 )
+
+type responseBodyWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w responseBodyWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
+func tracingMiddleware() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		tracer := otel.Tracer("test-collector.monitoring.middleware")
+		ctx := otel.GetTextMapPropagator().Extract(c.Request.Context(), propagation.HeaderCarrier(c.Request.Header))
+		ctx, span := tracer.Start(ctx, fmt.Sprintf("test-collector.%s", c.Request.RequestURI))
+
+		c.Set("OtelTraceContext", ctx)
+
+		defer span.End()
+
+		span.SetAttributes(
+			attribute.String("http.route", c.Request.RequestURI),
+			attribute.String("http.method", c.Request.Method),
+			attribute.String("http.path", c.Request.URL.Path),
+		)
+
+		writer := &responseBodyWriter{
+			body:           bytes.NewBufferString(""),
+			ResponseWriter: c.Writer,
+		}
+		c.Writer = writer
+		c.Next()
+
+		span.SetAttributes(
+			attribute.String("http.response.body", writer.body.String()),
+			attribute.Int("http.status_code", writer.Status()),
+		)
+	}
+}
 
 func setupOtel() (func(), error) {
 	ctx := context.Background()
@@ -56,12 +101,23 @@ func main() {
 	}
 	defer shutdownFunc()
 
-	log.Info().Msg("Hello Worrld")
-
 	tracer := otel.Tracer("test-collector.main")
 	_, mainSpan := tracer.Start(
 		context.Background(),
 		"app.main.main",
 	)
 	mainSpan.End()
+
+	g := gin.New()
+	g.Use(tracingMiddleware())
+	g.GET("/health", healthcheck)
+
+	log.Fatal().Err(g.Run(":8080"))
+}
+
+func healthcheck(c *gin.Context) {
+	traceContext := c.MustGet("OtelTraceContext")
+	_, span := otel.Tracer("test-collector.main").Start(traceContext.(context.Context), "app.healthcheck")
+	defer span.End()
+	c.JSON(http.StatusOK, map[string]bool{"ok": true})
 }
